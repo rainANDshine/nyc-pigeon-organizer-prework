f69396852e8357d77c8986c74cc1ccdad2e6f565
Shun Yao <ysmiracle@hotmail.com>
2018-03-31 17:51:38 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,10 +5,26 @@ def nyc_pigeon_organizer(data)
   end
   list.flatten!.uniq!
   
-  pigeon_list = {}
+  hash = {}
   list.each do |element|
-    pigeon_list[element]
+    hash[element] = {}
   end
   
+  data.each do |key, data|
+    data.each do |data_key, data_data|
+      data_data.each do |element|
+        hash.each do |name, attribute|
+          if element == name
+            if hash[name][key] == nil
+              hash[name][key] = [data_key.to_s]
+            else
+              hash[name][key] << data_key.to_s
+            end
+          end
+        end
+      end
+    end
+  end
   
+  hash
 end
