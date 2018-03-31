135d86f9f9dc0a07ef9e2f5b93574c77941400c2
Shun Yao <ysmiracle@hotmail.com>
2018-03-31 17:51:53 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,30 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  list = []
+  data.each do |key, data|
+    list << data.values
+  end
+  list.flatten!.uniq!
+  
+  hash = {}
+  list.each do |element|
+    hash[element] = {}
+  end
+  
+  data.each do |key, data|
+    data.each do |data_key, data_data|
+      data_data.each do |element|
+        hash.each do |name, attribute|
+          if element == name
+            if hash[name][key] == nil
+              hash[name][key] = [data_key.to_s]
+            else
+              hash[name][key] << data_key.to_s
+            end
+          end
+        end
+      end
+    end
+  end
+  
+  hash
 end
